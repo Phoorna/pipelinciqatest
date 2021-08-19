9e4384d5deffdb2d58d7c8302e432df74c2b38f8
Ravichandran <PRavichandran@teampurpose.com>
2021-08-19 12:59:44 -0400

    testing

---
+++ qaci/src/main/java/qaci/pipelincaforqa.java
@@ -0,0 +1,12 @@
+package qaci;
+
+public class pipelincaforqa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("test pipeline 1");
+
+	}
+
+}
